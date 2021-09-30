ccee0abf487a220f3a0b7e34a924f63624ecf1b1
Milind <milindchaudhary80@gmail.com>
2021-09-30 21:46:53 +0530

    Function defined to find color

--- Sketching_Main code.py
+++ Sketching_Main code.py
@@ -8,8 +8,16 @@ cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10,150)   #brightness
 
+
+def findColor(img):              # a function is defined to find color
+    imgHSV = cv2.cvtColor(imgResize, cv2.COLOR_BGR2HSV)
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    cv2.imshow("img", mask)               #to display the mask
+
 while True:
     success, img = cap.read()
     cv2.imshow("Result",img)
     if cv2.waitKey(1) & 0xFF == ord('q'):           #exit frame if pressed q
-        break 
+        break
