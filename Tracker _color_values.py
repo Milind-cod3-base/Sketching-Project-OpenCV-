19353ab3d1ec4ed9c85b4c3d4d69a07c01f7e0fb
Milind <milindchaudhary80@gmail.com>
2021-10-01 09:48:06 +0530

    Tracker bars introduced with values

--- Tracker _color_values.py
+++ Tracker _color_values.py
@@ -9,3 +9,16 @@ cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10,150)                  #frame dimensions set
 
+def empty(a):
+    pass
+
+
+cv2.namedWindow("HSV")                 #HSV window defined and sized
+cv2.resizeWindow("HSV", 640, 240)
+cv2.createTrackbar("HUE Min", "HSV", 0, 179, empty)
+cv2.createTrackbar("SAT Min", "HSV", 0, 255, empty)      #keeping the minimum values first
+cv2.createTrackbar("VALUE Min", "HSV", 0, 255, empty)
+
+cv2.createTrackbar("HUE Max", "HSV", 179, 179, empty)
+cv2.createTrackbar("SAT Max", "HSV", 255, 255, empty)    #and maximum values later
+cv2.createTrackbar("VALUE Max", "HSV", 255, 255, empty)
